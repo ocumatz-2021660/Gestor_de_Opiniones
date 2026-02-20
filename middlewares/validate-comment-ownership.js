fee8fced42ab4f4e95018895d78b4efff63fb30d
ocumatz-2021660 <ocumatz-2021660@kinal.edu.gt>
2026-02-20 09:03:56 -0600

    fix: corrigiendo comparacion de valores (usuario no esta en DB)

--- middlewares/validate-comment-ownership.js
+++ middlewares/validate-comment-ownership.js
@@ -1,32 +1,23 @@
 import Comment from '../src/comment/comment.model.js';
 
-//para verificar que el dueño de la publicacion si sea el que realizara el proceso
-export const validateCommentOwnership = async (req, res, next) =>{
-    try{
+export const validateCommentOwnership = async (req, res, next) => {
+    try {
         const { id } = req.params;
-        const uid = req.user._id; //usuario de la autenticacion (JWT)
+        const loggedUserId = String(req.userId).trim();
 
         const comment = await Comment.findById(id);
-        if(!comment){
-            return res.status(404).json({
-                success: false,
-                message: 'comentario no encontrado',
-            });
-        }
-        //Convierte los valores en texto y los compara para permitir el paso si es o no es el dueño de la publicacion
-        if(comment.author_comment.toString() !== uid.toString()) {
+        if (!comment) return res.status(404).json({ message: 'Comentario no encontrado' });
+
+        // Si el ID guardado no es igual al del token, rechaza
+        if (String(comment.author_comment).trim() !== loggedUserId) {
             return res.status(403).json({
-                success: falsae,
-                message: 'Error: you dont have authorization to perform this action',                
+                success: false,
+                message: 'No tienes permiso: No eres el autor de este comentario'
             });
         }
-        //continua con el processo
+
         next();
-    }catch(error){
-        return res.status(500).json({
-            success: false,
-            message: 'Error verificando al dueño de la publicacion ',
-            error: error.message,
-        });
+    } catch (error) {
+        res.status(500).json({ message: 'Error de validación', error: error.message });
     }
-}
+};
