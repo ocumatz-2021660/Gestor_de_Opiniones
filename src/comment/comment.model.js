6756af99104e52bbf9602af4c88adb0e87dbc823
ocumatz-2021660 <ocumatz-2021660@kinal.edu.gt>
2026-02-19 21:41:09 -0600

    feat: agregando modelo de comentarios (comment)

---
+++ src/comment/comment.model.js
@@ -0,0 +1,32 @@
+'use strict';
+
+import mongoose, { version } from 'mongoose';
+
+const commentSchema = new mongoose.Schema(
+    {
+        author_comment:{
+            type: String,
+            ref: 'User',
+            required: [true,'The author of comment is required'],
+        },
+        publication_comment: {
+            type: String,
+            ref: 'Publication',
+            required: [true,'The id of publication is required'],
+        },
+        content_comment:{
+            type: String,
+            maxlength: 250,
+            required: [true, 'The content of comment is required'],
+        },
+        comment_raiting:{
+            type: Number,
+            min: 1,
+            max: 5,
+            required: [true, 'The rating of comment is required'],
+        }
+    },{
+        timestamps: true,
+        versionKey: false
+    }
+);
